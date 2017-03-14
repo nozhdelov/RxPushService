30dcce7572a7b91201853ad42d316e17326047a3
nozhdelov <ivan_nozhdelov@yahoo.com>
2017-03-14 08:01:21 +0200

    some statistics

--- src/Storage.js
+++ src/Storage.js
@@ -117,7 +117,7 @@ Storage.prototype.getRegistrations = function(project, userId){
 
 Storage.prototype.incrDayMessageCount = function(project){
         var date = new Date();
-        var key = 'messagesPerDay::' + date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+        var key = project + '::messagesPerDay::' + date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
         this.client.incr(key);
 }
 
